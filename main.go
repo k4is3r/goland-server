40724390f7658a0d3bf3fd27f35a05daedf335a3
Eduardo Imery <ejwimery@gmail.com>
2019-11-27 23:18:12 -0600

    fixing problems with the serve

--- main.go
+++ main.go
@@ -9,16 +9,16 @@ func index_handler(w http.ResponseWriter, r *http.Request){
  fmt.Fprintf(w,"<h1>Whoa, Go is neat and here!</h1>")
 }
 
-func about_handler(w, http.ResponseWriter, r *http.Request){
+func about_handler(w http.ResponseWriter, r *http.Request){
   fmt.Fprintf(w,"<h1> We r in About page</h1>")
 }
 
 
 func main(){
-  http.HandleFunc("/", index_handler)
-  http.HandleFunc("/about",about_handler)
-  http.ListenAndServe(":8000",nil)
   fmt.Println("Server is running on port 8000")
+  http.HandleFunc("/", index_handler)
+  http.HandleFunc("/about/",about_handler)
+  http.ListenAndServe(":8000",nil)
 }
 
 
