132ad0f8dd12717aa773f4e9908c9f54520c0f2a
Eduardo Imery <ejwimery@gmail.com>
2019-11-27 23:07:20 -0600

    creating new handler for the page about

--- main.go
+++ main.go
@@ -6,12 +6,19 @@ import(
 )
 
 func index_handler(w http.ResponseWriter, r *http.Request){
- fmt.Fprintf(w,"<h1>Whoa, Go is neat and here!")
+ fmt.Fprintf(w,"<h1>Whoa, Go is neat and here!</h1>")
 }
+
+func about_handler(w, http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w,"<h1> We r in About page</h1>")
+}
+
 
 func main(){
   http.HandleFunc("/", index_handler)
+  http.HandleFunc("/about",about_handler)
   http.ListenAndServe(":8000",nil)
+  fmt.Println("Server is running on port 8000")
 }
 
 
